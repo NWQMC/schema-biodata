15c0ffd3c705631e43387d3a9707c59bc62a5a76
Mary Bucknell <mbucknell@usgs.gov>
2019-02-11 11:00:26 -0600

    Added changelogs for postgres and wqp set up. Added docker-compose file and Dockerfiles to create the db container and to run the liquibase scripts

---
+++ liquibase/dbInit/1_run_preload_liquibase.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# postgres to postgres db scripts
+${LIQUIBASE_HOME}/liquibase \
+	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
+	--changeLogFile=${LIQUIBASE_WORKSPACE}/postgres/postgres/changeLog.yml \
+	--driver=org.postgresql.Driver \
+	--url=jdbc:postgresql://BIODATA_Database:5432/postgres \
+	--username=postgres \
+	--password=${POSTGRES_PASSWORD} \
+	--logLevel=debug \
+	update \
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+	-DBIODATA_DATABASE_NAME=${BIODATA_DATABASE_NAME} \
+	-DBIODATA_OWNER=${BIODATA_OWNER} \
+	-DBIODATA_OWNER_PASSWORD=${BIODATA_OWNER_PASSWORD} \
+	-DBIODATA_SCHEMA_NAME=${BIODATA_SCHEMA_NAME} \
+	-DWQP_CORE_OWNER=${WQP_CORE_OWNER} \
+	-DWQP_CORE_OWNER_PASSWORD=${WQP_CORE_OWNER_PASSWORD} \
+	 > $LIQUIBASE_HOME/logs/liquibasePostgres.log
+
+# postgres to pubs db scripts
+${LIQUIBASE_HOME}/liquibase \
+	--classpath=${LIQUIBASE_HOME}/lib/${JDBC_JAR} \
+	--changeLogFile=${LIQUIBASE_WORKSPACE}/postgres/wqp/changeLog.yml \
+	--driver=org.postgresql.Driver \
+	--url=jdbc:postgresql://BIODATA_Database:5432/${BIODATA_DATABASE_NAME} \
+	--username=postgres \
+	--password=${POSTGRES_PASSWORD} \
+	--logLevel=debug \
+	update \
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+	-DBIODATA_OWNER=${BIODATA_OWNER} \
+	-DBIODATA_SCHEMA_NAME=${BIODATA_SCHEMA_NAME} \
+	 > $LIQUIBASE_HOME/logs/liquibaseWQP.log
